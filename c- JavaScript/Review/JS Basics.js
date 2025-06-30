9ec3b2b42dc7703b50f16d1946f8bdb8915fa947
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-06-30 08:31:30 +0800

    Create JS Basics.js

---
+++ c- JavaScript/Review/JS Basics.js
@@ -0,0 +1,28 @@
+//Console 
+console.log('JavaScript');
+console.log(2011);
+
+//Arithmetic operations
+console.log(21+3.5);
+console.log(2025-1969);
+console.log(65/240);
+console.log(0.2708*100);
+
+//Basic string concat
+console.log('Hello'+'World');
+console.log('Hello'+' '+'World');
+
+console.log('Chivalry is Dead'.length); //emz
+
+
+console.log('hello kitty'.toUpperCase());
+//removes white space
+console.log('    Removes whitespace   '.trim());
+
+//Built-in obj
+console.log(Math.random() * 100); //using math.random
+//to make output a whole number:
+console.log(Math.floor(Math.random() * 100)); //math.floor
+console.log(Math.ceil(43.8)); //returns the smallest int >= number so output is 44
+console.log(Number.isInteger(2017));  //number coercion topic; here it prints true
+
