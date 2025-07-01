f5b37db5c79bf95c4a7b6583e37873516adc6507
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-07-01 08:59:11 +0800

    Create JS Basics-2-Variables.js

---
+++ c- JavaScript/Review/JS Basics-2-Variables.js
@@ -0,0 +1,61 @@
+//Variables
+
+var favoriteFood ='pizza';
+var numOfSlices = 8;
+
+console.log(favoriteFood);
+console.log(numOfSlices);
+
+//Let
+let changeMe = true;
+console.log(changeMe);
+changeMe = false;
+console.log(changeMe);
+
+// Const
+const entree= 'Enchiladas'
+console.log(entree);
+
+//Arithmetic Operations
+let levelUp = 10;
+let powerLevel = 9001;
+let multiplyMe = 32;
+let quarterMe = 1152;
+
+// Math operations
+levelUp+=5;
+console.log(levelUp);
+powerLevel-=100;
+console.log(powerLevel);
+multiplyMe*=11;
+console.log(multiplyMe);
+quarterMe/=4;
+console.log(quarterMe);
+
+console.log('The value of levelUp:', levelUp); 
+console.log('The value of powerLevel:', powerLevel); 
+console.log('The value of multiplyMe:', multiplyMe); 
+console.log('The value of quarterMe:', quarterMe);
+
+//Inc and Dec
+let gainedDollar = 3;
+let lostDollar = 50;
+
+gainedDollar++;
+lostDollar--;
+
+//Concat
+var favoriteAnimal = "cat"
+console.log('My favorite animal: '+favoriteAnimal);
+
+//Interpolation
+const myName = 'Cin';
+const myCity = 'Milan';
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`);
+
+//Typeof
+let newVariable = 'Playing around with typeof.';
+console.log(typeof newVariable)
+newVariable = 1;
+console.log(newVariable);
+console.log(typeof newVariable);
