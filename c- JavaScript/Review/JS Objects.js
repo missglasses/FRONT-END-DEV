fbd8e211ab9bf205f732970cf287a98c5820cb91
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-07-05 06:51:14 +0800

    Update JS Objects.js

--- c- JavaScript/Review/JS Objects.js
+++ c- JavaScript/Review/JS Objects.js
@@ -1,1 +1,121 @@
 
+//Objects
+//create:
+let fasterShip = {
+  'Fuel Type': 'Turbo Fuel',
+  color: 'silver'
+};
+
+//acessing properties: 
+let spaceship = {
+  homePlanet: 'Earth',
+  color: 'silver',
+  'Fuel Type': 'Turbo Fuel',
+  numCrew: 5,
+  flightPath: ['Venus', 'Mars', 'Saturn']
+};
+
+//access
+var crewCount = spaceship.numCrew;
+var planetArray = spaceship.flightPath;
+
+//bracket notation
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  'Active Mission' : true,
+  homePlanet : 'Earth', 
+  numCrew: 5
+ };
+
+let propName =  'Active Mission';
+
+var isActive = spaceship['Active Mission'];
+console.log(spaceship[propName]); //true
+
+//property assingment/ reassignment
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  homePlanet : 'Earth',
+  color: 'silver',
+  'Secret Mission' : 'Discover life outside of Earth.'
+};
+
+spaceship.color = 'glorious gold';
+spaceship.numEngines = 4; 
+delete spaceship['Secret Mission']; 
+
+//Methods
+let retreatMessage = 'We no longer wish to conquer your planet. It is full of dogs, which we do not care for.';
+
+let alienShip = {
+  retreat(){
+  console.log(retreatMessage)
+  },
+
+  takeOff(){
+    console.log('Spim... Borp... Glix... Blastoff!')
+  }
+};
+
+alienShip.retreat();
+alienShip.takeOff();
+
+//nested 
+let spaceship = {
+  passengers: [{name: 'Gran'}], //assign name 
+  telescope: {
+    yearBuilt: 2018,
+    model: "91031-XLT",
+    focalLength: 2032 
+  },
+  crew: {
+    captain: { 
+      name: 'Sandra', 
+      degree: 'Computer Engineering', 
+      encourageTeam() { console.log('We got this!') },
+     'favorite foods': ['cookies', 'cakes', 'candy', 'spinach'] }
+  },
+  engine: {
+    model: "Nimbus2000"
+  },
+  nanoelectronics: {
+    computer: {
+      terabytes: 100,
+      monitors: "HD"
+    },
+    'back-up': {
+      battery: "Lithium",
+      terabytes: 50
+    }
+  }
+}; 
+
+let capFave = spaceship.crew.captain['favorite foods'][0]; //access
+
+let firstPassenger = spaceship.passengers[0];//here 
+
+//pass by reference
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  homePlanet : 'Earth'
+};
+
+
+let greenEnergy = obj => {
+  obj['Fuel Type'] = 'avocado oil';
+}
+
+let remotelyDisable = obj => {
+  obj.disabled = true;
+}
+
+greenEnergy(spaceship);
+remotelyDisable(spaceship);
+
+console.log(spaceship)
+
+//Loop through objects
+for (let role in spaceship.crew) {
+    const crewMember = spaceship.crew[role]; 
+    console.log(`${crewMember.name}: ${crewMember.degree}`);
+}
