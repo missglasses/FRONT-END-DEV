a61794da6c7966773a2d0c07a08b9a9f5bdd40d5
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-07-07 15:20:38 +0800

    Update Array Type Notations.ts

--- Web Security Basics/Web Components/Type Checkers/TypeScript/Array Type Notations.ts
+++ Web Security Basics/Web Components/Type Checkers/TypeScript/Array Type Notations.ts
@@ -3,3 +3,15 @@ let bestNumbers: number[] = [7,77,4]; //<-datatype[]:  number []
 let bestLunches: string[] = ['chicken soup', 'non-chicken soup'];
 let bestBreakfasts: string[]= ['scrambled eggs', 'oatmeal', 'tamago kake gohan', 'any kind of soup'];
 let bestBooleans: boolean[] = [true, false];
+
+//in multiD:
+// Arrays:
+let bestNumbers: number[] = [7,77,4]; 
+let bestLunches: string[] = ['chicken soup', 'non-chicken soup'];
+let bestBreakfasts: string[]= ['fasting', 'oatmeal', 'tamago kake gohan', 'any kind of soup'];
+let bestBooleans: boolean[] = [true, false];
+
+// Multidimensional Arrays:
+let bestMealPlan:string[][] = [bestLunches, bestBreakfasts, ['baked potato', 'mashed potato']]; //<--data type[][]
+let bestBooleansTwice: boolean[][] = [bestBooleans, bestBooleans];
+let numbersMulti: number[][][] = [ [[1],[2,3]], [[7],bestNumbers] ];
