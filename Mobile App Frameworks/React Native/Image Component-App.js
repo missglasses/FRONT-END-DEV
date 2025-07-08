2edc969e94b9e3aed50aaa4af67fc06e77bbd045
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-07-08 12:38:12 +0800

    Update Image Component-App.js

--- Mobile App Frameworks/React Native/Image Component-App.js
+++ Mobile App Frameworks/React Native/Image Component-App.js
@@ -1,1 +1,13 @@
 
+import React from 'react';
+import { Image, View } from 'react-native';
+const image = require('./react-native.jpg'); //shows the react image
+
+const App = () => (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+
+    <Image style={{ width: 100, height: 100 }} source={image} /> //<--here
+  </View>
+);
+
+export default App;
