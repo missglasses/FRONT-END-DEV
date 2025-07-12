bb987b12dffd2d2548c418a8c01f5850e0baa321
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-07-12 09:54:00 +0800

    Create string-concat.kt

---
+++ Mobile App Development/Native/Kotlin/Intro/string-concat.kt
@@ -0,0 +1,7 @@
+fun main() {
+  val dog = "Toto"
+  val state = "Kansas"
+  val movie = "The Wizard of Oz"
+
+println(dog+", "+ "I've a feeling we're not in "+state+" anymore. - "+movie+", 1939")
+}
