98105c9466ae75362927cad26adc9906a4e5be2e
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-07-13 09:24:38 +0800

    Create built-ins.kt

---
+++ Mobile App Development/Native/Kotlin/Data Types and Variables/Text Variables/built-ins.kt
@@ -0,0 +1,8 @@
+fun main() {
+  var word = "supercalifragilisticexpialidocious"
+  
+  word = word.capitalize() //<-- cap
+  var wordSize = word.length  //<-get length
+
+  println("$word has $wordSize letters.")
+}
