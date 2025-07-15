c07b055253773840cd75b7f0a6c68cdb8555e9de
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-07-16 07:21:41 +0800

    Create acess-list.kt

---
+++ Mobile App Development/Native/Kotlin/Collections/acess-list.kt
@@ -0,0 +1,7 @@
+fun main() {
+  
+  var unitedStatesMarathons = listOf("Boston Marathon", "Bank of America Chicago Marathon", "TCS NYC Marathon", "Marine Corps Marathon") //Bank of America Chicago Marathon
+
+  println(unitedStatesMarathons[1]) //access an element 
+
+}
