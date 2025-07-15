bf956ccfc4072c528e901f7491f2c48b68ce5b82
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-07-16 07:25:00 +0800

    Create mutable-list.kt

---
+++ Mobile App Development/Native/Kotlin/Collections/mutable-list/mutable-list.kt
@@ -0,0 +1,11 @@
+fun main() {
+
+//func with variable mutableListOf
+  var openBroadwayShows = mutableListOf("The Lion King", "Chicago", "The Cher Show", "Les Miserables")
+
+//access
+  openBroadwayShows[2] = "Wicked"
+
+  println(openBroadwayShows)
+  
+}
