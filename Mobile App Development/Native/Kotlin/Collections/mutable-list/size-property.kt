11d5051e45da5e7ab9c1013f68ce31f9c3d6f903
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-07-16 07:28:25 +0800

    Create size-property.kt

---
+++ Mobile App Development/Native/Kotlin/Collections/mutable-list/size-property.kt
@@ -0,0 +1,6 @@
+fun main() {
+  var fruitTrees = mutableListOf("Apple", "Plum", "Pear", "Cherry")
+
+//use str template
+  println("I am growing ${fruitTrees.size} different types of fruit in my garden.")
+}
