713f7a9102b1b8150c398b083e734d02e973f3b5
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-07-17 08:54:38 +0800

    Create immutable-map.kt

---
+++ Mobile App Development/Native/Kotlin/Collections/Map/immutable-map.kt
@@ -0,0 +1,7 @@
+fun main() {
+
+  var codeysMap = mapOf("valley" to "scorpions", "creek" to "snakes", "forest" to "bears")
+
+  println(codeysMap) //{valley=scorpions, creek=snakes, forest=bears}
+  
+}
